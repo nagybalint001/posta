3b2160b626099dd3df6d8d16d95bfe0f604031e1
Bordák Tamás <tamas@webtar.hu>
2017-10-30 04:04:50 +0100

    change password

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -57,6 +57,7 @@ module.exports = {
     || req.body.name != req.session.user.name){
       return renderform("Invalid user!")
     }
+    
     var userNewData = {};
 
     if(req.body.fullname && req.body.fullname != '')
@@ -64,6 +65,7 @@ module.exports = {
 
     userNewData.email = req.body.email || '';
     userNewData.phone = req.body.phone || '';
+
 
     var updateUser = function (newdata) {
       User.update({ id: req.body.id, name: req.body.name }, newdata, function (err, results) {
@@ -74,15 +76,32 @@ module.exports = {
         if (results) {
           //refresh session user
           req.session.user = results[0];
-          renderForm("alles OK");
+          return renderForm("alles OK");
         }
       });
     }
-    // TODO check old password to update
-    // userNewData.newpassword = ...
-    // userNewData.newpassword - clear text 
-    //    (user model beforeUpdate - hash, change)
-    updateUser(userNewData);
+    if(req.body.newpassword !== req.body.newpassword2)
+		return renderForm("passwords must match")
+	else if(req.body.oldpassword)
+      User.findOne({id: req.body.id}).exec((err, user) =>
+      {
+        if(!user || err)
+          return console.log("error");
+	    User.checkPassword(user, req.body.oldpassword, (err, valid) =>
+        {
+          if(!valid )
+            return renderForm("password incorect");
+          else if(!req.body.newpassword)
+            return renderForm("enter password");
+          else
+            userNewData.newpassword = req.body.newpassword;
+          return updateUser(userNewData);
+        })
+      })
+	else
+      return updateUser(userNewData);
+
+    // TODO fix stupid structure of code
   },
     
   logout: function(req, res) {
