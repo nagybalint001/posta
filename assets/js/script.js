8ab0df52904ffb42872b76581e2fd8fac574a6ad
Bordák Tamás <tamas@webtar.hu>
2017-11-20 09:21:08 +0100

    async type get fix

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,25 @@
 var typeInputs = {    
 }
 
-io.socket.get('/api/type', function (body, response) {
-    body.forEach(function(item) {
-        typeInputs[item.name] = item.fields;
-    })
-});
-
 function changePackageInputs(selected){
-    var els = document.querySelectorAll('.form-group.input-more .form-control');
-    if(typeof typeInputs[selected] === 'undefined')
-        return;
-    els.forEach(function(el){
-        if(typeInputs[selected].indexOf(el.id) >= 0){
-            el.removeAttribute('readonly');
-            el.parentElement.parentElement.classList.remove("hidden");
-        }
-        else{
-            el.setAttribute('readonly', '');
-            el.parentElement.parentElement.classList.add("hidden");
-        }
-    });
+	io.socket.get('/api/type', function (body, response) {
+		body.forEach(function(item) {
+			typeInputs[item.name] = item.fields;
+		})
+
+		var els = document.querySelectorAll('.form-group.input-more .form-control');
+		if(typeof typeInputs[selected] === 'undefined')
+			return;
+		els.forEach(function(el){
+			if(typeInputs[selected].indexOf(el.id) >= 0){
+				el.removeAttribute('readonly');
+				el.parentElement.parentElement.classList.remove("hidden");
+			}
+			else{
+				el.setAttribute('readonly', '');
+				el.parentElement.parentElement.classList.add("hidden");
+			}
+		});
+
+	});
 }
