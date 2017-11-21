bc32249bbed003ca8245df6bf14ed40b05fdcc82
Henczidai Bálint <henczidaibalint@gmail.com>
2017-11-21 09:11:43 +0100

    IE reactive form FIX

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,7 +10,7 @@ function changePackageInputs(selected){
 		var els = document.querySelectorAll('.form-group.input-more .form-control');
 		if(typeof typeInputs[selected] === 'undefined')
 			return;
-		els.forEach(function(el){
+		[].slice.call(els).forEach(function(el){
 			if(typeInputs[selected].indexOf(el.id) >= 0){
 				el.removeAttribute('readonly');
 				el.parentElement.parentElement.classList.remove("hidden");
