c1f4c586ec58da552b693c311c2270b0f893da99
Henczidai Bálint <henczidaibalint@gmail.com>
2017-11-22 10:59:57 +0100

    multi stat test

--- api/controllers/StatisticsController.js
+++ api/controllers/StatisticsController.js
@@ -1,8 +1,23 @@
 module.exports = {
     renderPage: function(req, res){
-        Package.find({ select: ['date', 'value'], where: {'value':{'>':0}}})
+        /*Package.find({ select: ['date', 'value'], where: {'value':{'>':0}}})
         .exec(function(err, result){
             res.view("statistics", {data:result});
+        });*/
+
+        Promise.all([
+            Package.find({ select: ['date', 'value'], where: {'value':{'>':0}}}),
+            Package.find({ select: ['date', 'total']})
+        ])
+        .then(function(arr){
+            var data = {
+                dateValue : arr[0],
+                dateTotal : arr[1]
+            }
+            res.view("statistics", {data});
+        })
+        .catch(function(reason){
+            console.log(reason);
         });
     }
 };
