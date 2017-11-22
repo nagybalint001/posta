29c4ac49f7c14c47d1539cfe890da6289606fa7e
Henczidai Bálint <henczidaibalint@gmail.com>
2017-11-22 23:13:05 +0100

    some pie chart

--- api/controllers/StatisticsController.js
+++ api/controllers/StatisticsController.js
@@ -4,15 +4,30 @@ module.exports = {
         .exec(function(err, result){
             res.view("statistics", {data:result});
         });*/
-
+        var groupByCount = function(xs, key) {
+            return xs.reduce(function(rv, x) {
+                (rv[x[key]] = rv[x[key]] || 0);
+                rv[x[key]]++;
+                return rv;
+            }, {});
+        };
         Promise.all([
+            Package.find({select:'dir'}).then(x => groupByCount(x, 'dir')),
+            Package.find({select:'type'}).then(x => groupByCount(x, 'type')),
+            Package.find({select:'administrator'}).then(x => groupByCount(x, 'administrator')),
+            Package.find({select:'division'}).then(x => groupByCount(x, 'division')),
             Package.find({ select: ['date', 'value'], where: {'value':{'>':0}}}),
             Package.find({ select: ['date', 'total']})
         ])
         .then(function(arr){
             var data = {
-                dateValue : arr[0],
-                dateTotal : arr[1]
+                dirCount : Object.keys(arr[0]).map(x => ({dir:x, count: arr[0][x]})),
+                typeCount : Object.keys(arr[1]).map(x => ({type:x, count: arr[1][x]})),
+                administratorCount : Object.keys(arr[2]).map(x => ({administrator:x, count: arr[2][x]})),
+                divisionCount : Object.keys(arr[3]).map(x => ({division:x, count: arr[3][x]})),
+                dateValue : arr[4],
+                dateTotal : arr[5],
+                raw : arr
             }
             res.view("statistics", {data});
         })
