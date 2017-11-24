ddecb97aa35772c05a7737c9166198b20aa10339
Nagy Bálint <balint.boy@gmail.com>
2017-11-24 12:14:20 +0100

    Revert "Swagger integration"
    This reverts commit ec9a6838176de1734a952f562eecb04903c439c1.

--- config/installedHooks.js
+++
@@ -1,6 +0,0 @@
-module.exports.installedHooks = {
-    "sails-swagger": {
-       // load the hook into sails.hooks.swagger instead of sails.hooks['sails-swagger']
-       "name": "swagger"
-    }
- };
--- config/swagger.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-module.exports.swagger = {
-  /**
-   * require() the package.json file for your Sails app.
-   */
-  pkg: require('../package')
-};
