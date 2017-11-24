e43b35cd3eae61f59abee5903f71704097ac20dd
Henczidai Bálint <henczidaibalint@gmail.com>
2017-11-24 14:04:00 +0100

    stat impr

--- api/controllers/StatisticsController.js
+++ api/controllers/StatisticsController.js
@@ -16,8 +16,8 @@ module.exports = {
             Package.find({select:'type'}).then(x => groupByCount(x, 'type')),
             Package.find({select:'administrator'}).then(x => groupByCount(x, 'administrator')),
             Package.find({select:'division'}).then(x => groupByCount(x, 'division')),
-            Package.find({ select: ['date', 'value'], where: {'value':{'>':0}}}),
-            Package.find({ select: ['date', 'total']})
+            Package.find({ select: ['subject', 'value'], where: {'value':{'>':0}}}),
+            Package.find({ select: ['subject', 'total']})
         ])
         .then(function(arr){
             var data = {
@@ -25,8 +25,8 @@ module.exports = {
                 typeCount : Object.keys(arr[1]).map(x => ({type:x, count: arr[1][x]})),
                 administratorCount : Object.keys(arr[2]).map(x => ({administrator:x, count: arr[2][x]})),
                 divisionCount : Object.keys(arr[3]).map(x => ({division:x, count: arr[3][x]})),
-                dateValue : arr[4],
-                dateTotal : arr[5],
+                subjectValue : arr[4],
+                subjectTotal : arr[5],
                 raw : arr
             }
             res.view("statistics", {data});
